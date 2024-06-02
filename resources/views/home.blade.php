51170bcb67558aa55111102d414ea7140f19b6d8
LesterArana <laranam3@miumg.edu.gt>
2024-06-01 20:53:21 -0600

    primer comit, subiendo proyecto

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+@extends('adminlte::page')
+
+@section('title', 'Sistema Inserpro')
+
+@section('content_header')
+    <h1>Inserpro</h1>
+@stop
+
+@section('content')
+    <p>SISTEMA EN MANTENIMIENTO.</p>
+@stop
+
+@section('css')
+    {{-- Add here extra stylesheets --}}
+    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+@stop
+
+@section('js')
+    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
+@stop
